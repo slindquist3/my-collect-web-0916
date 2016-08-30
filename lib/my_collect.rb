10a01a1ff54ba5d7b31cb116cfa94112dbdfebfe
slindquist3 <slindquist3@gmail.com>
2016-08-30 16:54:21 -0400

    Built my_collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  new_collection = []
+  counter = 0
+  while counter < collection.length
+  new_collection << yield(collection[counter])
+  counter = counter + 1
+  end
+  new_collection
+end
